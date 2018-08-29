b0da09ffeca67c2f0dbf4b8a931cf068fe386761
Denis Anikin <ad@xfenix.ru>
2018-08-29 04:05:30 +0300

    Some update

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,31 @@
-const express = require('express');
-const os = require('os');
+const express = require('express')
+const os = require('os')
+const redis = require('redis')
+require('dotenv').config()
 
-const app = express();
+const client = redis.createClient()
+const app = express()
+app.use(express.static('dist'))
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+
+app.get(
+    '/api/getUsername/',
+    (req, res) => {
+        return res.send({ username: os.userInfo().username })
+    }
+)
+
+app.get(
+    '/api/food/',
+    (req, res) => {
+        client.set('lol', 1)
+        return res.json({a: 1})
+    }
+)
+
+app.listen(
+    8080, () => {
+        client.select(process.env.REDIS_DB ? process.env.REDIS_DB : 1)
+        console.log('Listening on port 8080!')
+    }
+)
