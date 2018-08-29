e9877faf3a546573393623817126a3b07c834684
Denis Anikin <ad@xfenix.ru>
2018-08-29 04:44:24 +0300

    Current food basic

--- src/client/CurrentFood.js
+++ src/client/CurrentFood.js
@@ -3,6 +3,12 @@ import React, { Component } from "react";
 
 export default class CurrentFood extends Component {
     render() {
-        return <div></div>
+        return <div className="current">
+            <h1 className="current__title"></h1>
+            <div className="current__buttons">
+                <a href="#" className="current__button current__button_ok">Ест :)</a>
+                <a href="#" className="current__button current__button_notok">Не ест :(</a>
+            </div>
+        </div>
     }
 }
