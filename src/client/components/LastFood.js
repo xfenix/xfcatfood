b15cf40611d0e93e16438e86d773abc201d2cf02
Denis <ad@xfenix.ru>
2018-09-06 21:34:54 +0300

    Update LastFood.js
    Work in progress

--- src/client/components/LastFood.js
+++ src/client/components/LastFood.js
@@ -20,8 +20,10 @@ export default class LastFood extends Component {
                 <h4 className="lastfood__title">Последние банки:</h4>
                 <ul className="lastfood__list">
                     {this.state.lastCans ? (
-                        this.state.lastCans.map(function(name, index){
-                            return <li className="lastfood__item" key={index}>{name}</li>;
+                        this.state.lastCans.map(function(item, index){
+                            return <li className="lastfood__item" key={index}>
+                                {item.title} <span class="lastfood__date">{item.date}</span>
+                            </li>;
                         })
                     ) : (
                         <li className="lastfood__item"></li>
